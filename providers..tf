76dc45ac65f2ec9fc66f39d6f6be3a10a13de546
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2021-06-27 02:35:15 +0400

    feat: use remote state file
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- providers..tf
+++ providers..tf
@@ -1,4 +1,17 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-practice"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_practice_lock"
+
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
